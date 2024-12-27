6a3032cfa34cdec63e94383bad1e1ee4152d16b2
Morphy55555 <learningthings1996@gmail.com>
2024-12-27 12:44:52 +0800

    Centered grid and button, ustilising flexBox and margin 'auto'

--- main.js
+++ main.js
@@ -11,18 +11,17 @@ for (i = 1; i <= 16 * 16; i++) {
 
 function colorChange() {
 const divs = document.querySelectorAll('.divs');
-divs.forEach((div) => {
-    div.addEventListener('mouseover', () =>
-    div.style.backgroundColor = 'purple');
-    
-});
+    divs.forEach((div) => {
+        div.addEventListener('mouseover', () =>
+        div.style.backgroundColor = 'purple'); 
+        });
 };
 colorChange();
 
 const buttons = document.querySelector('button');
 buttons.addEventListener('click', () => {
     getGridSize = prompt("Enter a number, to choose the grid size, eg '10' for a 10x10 grid");
-    if (getGridSize > 0) {
+    if (getGridSize > 0 && getGridSize < 100) {
         body.removeChild(container);
         body.appendChild(newContainer);
         for (i = 1; i <= getGridSize * getGridSize; i++) {
