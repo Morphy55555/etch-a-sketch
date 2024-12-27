2da4d7c1f95c9ab1bab6f2904767dd44bb29b9d5
Morphy55555 <learningthings1996@gmail.com>
2024-12-27 10:41:25 +0800

    Added function to allow each div to be changed color when you mouse-over. Done by utilising querySelectorAll to create a node list and forEach to iterate through the node list

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-const container = document.querySelector('.container')
+
+const container = document.querySelector('.container');
 for (i = 1; i <= 16 * 16; i++) {
-const divCreate = document.createElement("div");
-divCreate.className = 'divs';
-container.appendChild(divCreate);
+    const gridCreate = document.createElement("div");
+    gridCreate.className = 'divs';
+    container.appendChild(gridCreate);
 }
+const divs = document.querySelectorAll('.divs');
+divs.forEach((div) => {
+    div.addEventListener('mouseover', () =>
+    div.style.backgroundColor = 'purple');
+    // alert('clicked lad'));
+});
+
