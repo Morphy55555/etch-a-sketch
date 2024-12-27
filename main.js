e36326b814c747c05a97ce058bf4264a17b507c5
Morphy55555 <learningthings1996@gmail.com>
2024-12-27 12:26:51 +0800

    Added functionality for user to enter amount of squares for the grid to be, removing old sqaures

--- main.js
+++ main.js
@@ -1,14 +1,33 @@
-
+const body = document.querySelector('body');
 const container = document.querySelector('.container');
+const newContainer = document.querySelector('.newContainer');
 for (i = 1; i <= 16 * 16; i++) {
     const gridCreate = document.createElement("div");
     gridCreate.className = 'divs';
     container.appendChild(gridCreate);
 }
+
+// userColorChoice = prompt("select a colour, spell it right lad");
+
 const divs = document.querySelectorAll('.divs');
 divs.forEach((div) => {
     div.addEventListener('mouseover', () =>
     div.style.backgroundColor = 'purple');
-    // alert('clicked lad'));
+    
 });
+
+const buttons = document.querySelector('button');
+buttons.addEventListener('click', () => {
+    getGridSize = prompt("Enter a number, to choose the grid size, eg '10' for a 10x10 grid");
+    if (getGridSize > 0) {
+        body.removeChild(container);
+        body.appendChild(newContainer);
+        for (i = 1; i <= getGridSize * getGridSize; i++) {
+        const gridCreate = document.createElement("div");
+        gridCreate.className = 'divs';
+        newContainer.appendChild(gridCreate);
+        }
+    }
+});
+
 
